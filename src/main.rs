3689adff5552c936cf4e7e52f5f75c3bce0d0127
Stanly Johnson <stanlyjohnson@outlook.com>
2020-11-07 18:33:38 +0530

    setup tree

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#[derive(Debug)]
+pub struct Node<'a> {
+    val : &'a str,
+    weight : u32,
+    children : Option<Vec<Node<'a>>>
+}
+
+impl <'a> Node <'a> {
+    pub fn add_data(&mut self, children : Option<Vec<Node<'a>>>){
+        self.children = children;
+    }
+    
+    pub fn get_winner(self) -> String {
+        "test".to_string()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut new_node = Node{val : "Total", weight : 10, children : None};
+    println!("{:?}", new_node);
+    let child_node = Node{val : "B", weight : 4, children : None};
+    let child_refs : Vec<Node> = vec![child_node];
+
+    new_node.add_data(Some(child_refs));
+
+    println!("{:?}", new_node);
+
 }
